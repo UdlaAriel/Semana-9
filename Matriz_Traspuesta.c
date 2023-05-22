a3e3472e7b6898ad601ff9a2d27123eab9c445e3
Ariel Mateo Anchapaxi Moreta <130407481+UdlaAriel@users.noreply.github.com>
2023-05-22 15:52:43 -0500

    Segundo cambio

---
+++ Matriz_Traspuesta.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int filas, columnas;    
+    scanf("%d",&filas);
+    scanf("%d",&columnas);
+
+    /*Definicion de matriz*/
+    int MATRIZ[50][50];
+
+
+
+    return 0;
+}
