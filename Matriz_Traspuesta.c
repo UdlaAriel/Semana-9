6e6d54f8bd84f048f5b69de416b04e4f780352d2
Ariel Mateo Anchapaxi Moreta <130407481+UdlaAriel@users.noreply.github.com>
2023-05-22 16:00:39 -0500

    Tercer cambio

--- Matriz_Traspuesta.c
+++ Matriz_Traspuesta.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main ()
 {
@@ -10,7 +11,34 @@ int main ()
     /*Definicion de matriz*/
     int MATRIZ[50][50];
 
+    for(int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            /*Funcion para numero randomico*/
+            int numero; //Variable donde se guardará nuestro numero aleatorio
+	
+	        srand(time(NULL)); //Generamos numero aleatorio en base al tiempo
+	        numero=rand() % 20;
 
+            MATRIZ[i][j] = numero;
+        }
+    }   
+
+    for(int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            printf(" %d ", MATRIZ[i][j]);
+
+            if(j == (columnas-1))
+            {
+                printf("\n");
+            }
+        }
+    }
+
+    puts(" ");
 
     return 0;
 }
